242e65da56c383228ff4ed84d082183ba9088380
Saksham <67902670+Sak-sham@users.noreply.github.com>
2024-03-30 18:30:35 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "Saksham" # write your name
+    age = "14" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route('/father-webpage', methods=["POST"])
+def father():
+    
+
+# define the route to mother webpage
+@app.route('/mother-webpage', methods=["POST"])
+def mother():
+
+# define the route to friends webpage
+@app.route('/friend-webpage', methods=["POST"])
+def friend():
+
+
+
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
